9bec0b3ccdf6da39c3127c678bd46f3d2aea9399
IsaiahW832 <33337852+IsaiahW832@users.noreply.github.com>
2019-02-17 07:04:30 -0600

    Add files via upload

---
+++ FRC_VL53L0X.cpp
@@ -0,0 +1,253 @@
+/*!
+ * @file Adafruit_VL53L0X.cpp
+ *
+ * @mainpage Adafruit VL53L0X time-of-flight sensor
+ *
+ * @section intro_sec Introduction
+ *
+ * This is the documentation for Adafruit's VL53L0X driver for the
+ * Arduino platform.  It is designed specifically to work with the
+ * Adafruit VL53L0X breakout: https://www.adafruit.com/product/3317
+ *
+ * These sensors use I2C to communicate, 2 pins (SCL+SDA) are required
+ * to interface with the breakout.
+ *
+ * Adafruit invests time and resources providing this open source code,
+ * please support Adafruit and open-source hardware by purchasing
+ * products from Adafruit!
+ *
+ * @section dependencies Dependencies
+ *
+ *
+ * @section author Author
+ *
+ * Written by Limor Fried/Ladyada for Adafruit Industries.
+ *
+ * @section license License
+ *
+ * BSD license, all text here must be included in any redistribution.
+ *
+ */
+
+#include "FRC_VL53L0X.h"
+
+#include <I2C.h>
+
+#define VERSION_REQUIRED_MAJOR  1 ///< Required sensor major version
+#define VERSION_REQUIRED_MINOR  0 ///< Required sensor minor version
+#define VERSION_REQUIRED_BUILD  2 ///< Required sensor build
+
+#define STR_HELPER( x ) #x ///< a string helper
+#define STR( x )        STR_HELPER(x) ///< string helper wrapper
+
+/**************************************************************************/
+/*!
+    @brief  Setups the I2C interface and hardware
+    @param  i2c_addr Optional I2C address the sensor can be found on. Default is 0x29
+    @param debug Optional debug flag. If true, debug information will print out via Serial.print during setup. Defaults to false.
+    @returns True if device is set up, false on any failure
+*/
+/**************************************************************************/
+bool FRC_VL53L0X::begin(frc::I2C::Port port = frc::I2C::Port::kOnboard, uint8_t i2c_addr, bool debug) {
+  int32_t   status_int;
+  int32_t   init_done        = 0;
+
+  uint32_t  refSpadCount;
+  uint8_t   isApertureSpads;
+  uint8_t   VhvSettings;
+  uint8_t   PhaseCal;
+
+  // Initialize Comms
+  pMyDevice->I2cDevAddr      =  VL53L0X_I2C_ADDR;  // default
+  pMyDevice->comms_type      =  1;
+  pMyDevice->comms_speed_khz =  400;
+
+  VL53L0X_i2c_init(port, VL53L0X_I2C_ADDR);
+
+  // unclear if this is even needed:
+  if( VL53L0X_IMPLEMENTATION_VER_MAJOR != VERSION_REQUIRED_MAJOR ||
+      VL53L0X_IMPLEMENTATION_VER_MINOR != VERSION_REQUIRED_MINOR ||
+      VL53L0X_IMPLEMENTATION_VER_SUB != VERSION_REQUIRED_BUILD )  {
+      if( debug ) {
+          std::cout << "Found " << VL53L0X_IMPLEMENTATION_VER_MAJOR << "." << VL53L0X_IMPLEMENTATION_VER_MINOR << "." << VL53L0X_IMPLEMENTATION_VER_SUB << " rev " << VL53L0X_IMPLEMENTATION_VER_REVISION << std::endl;
+          std::cout << "Requires " << VERSION_REQUIRED_MAJOR << "." << VERSION_REQUIRED_MINOR << "." << VERSION_REQUIRED_BUILD << std::endl;
+      }
+
+      Status = VL53L0X_ERROR_NOT_SUPPORTED;
+
+      return false;
+  }
+
+  Status = VL53L0X_DataInit( &MyDevice );         // Data initialization
+
+  if (! setAddress(i2c_addr) ) {
+    return false;
+  }
+
+  Status = VL53L0X_GetDeviceInfo( &MyDevice, &DeviceInfo );
+
+  if( Status == VL53L0X_ERROR_NONE )  {
+      if( debug ) {
+         std::cout << "VL53L0X Info:"  << std::endl;
+         std::cout << "Device Name: " <<  DeviceInfo.Name << ", Type: " << DeviceInfo.Type << ", ID: " << DeviceInfo.ProductId << std::endl;
+         std::cout << "Rev Major: " << DeviceInfo.ProductRevisionMajor << ", Minor: " << DeviceInfo.ProductRevisionMinor << std::endl;
+      }
+
+      if( ( DeviceInfo.ProductRevisionMinor != 1 ) && ( DeviceInfo.ProductRevisionMinor != 1 ) ) {
+          if( debug ) {
+              std::cout << "Error expected cut 1.1 but found " << DeviceInfo.ProductRevisionMajor << ',' <<DeviceInfo.ProductRevisionMinor << std::endl;
+          }
+
+          Status = VL53L0X_ERROR_NOT_SUPPORTED;
+      }
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      if( debug ) {
+          std::cout << "VL53L0X: StaticInit" << std::endl;
+      }
+      Status = VL53L0X_StaticInit( pMyDevice ); // Device Initialization
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      if( debug ) {
+          std::cout << "VL53L0X: PerformRefSpadManagement" << std::endl;
+      }
+
+      Status = VL53L0X_PerformRefSpadManagement( pMyDevice, &refSpadCount, &isApertureSpads ); // Device Initialization
+
+      if( debug ) {
+          std::cout << "refSpadCount = " << refSpadCount << ", isApertureSpads = " << isApertureSpads << std::endl;
+      }
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      if( debug ) {
+          std::cout << "VL53L0X: PerformRefCalibration" << std::endl;
+      }
+
+      Status = VL53L0X_PerformRefCalibration( pMyDevice, &VhvSettings, &PhaseCal );           // Device Initialization
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      // no need to do this when we use VL53L0X_PerformSingleRangingMeasurement
+      if( debug ) {
+          std::cout << "VL53L0X: SetDeviceMode" << std::endl;
+      }
+
+      Status = VL53L0X_SetDeviceMode( pMyDevice, VL53L0X_DEVICEMODE_SINGLE_RANGING );        // Setup in single ranging mode
+  }
+
+  // Enable/Disable Sigma and Signal check
+  if( Status == VL53L0X_ERROR_NONE ) {
+      Status = VL53L0X_SetLimitCheckEnable( pMyDevice, VL53L0X_CHECKENABLE_SIGMA_FINAL_RANGE, 1 );
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      Status = VL53L0X_SetLimitCheckEnable( pMyDevice, VL53L0X_CHECKENABLE_SIGNAL_RATE_FINAL_RANGE, 1 );
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      Status = VL53L0X_SetLimitCheckEnable( pMyDevice, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, 1 );
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      Status = VL53L0X_SetLimitCheckValue( pMyDevice, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, (FixPoint1616_t)( 1.5 * 0.023 * 65536 ) );
+  }
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+      return true;
+  } else {
+      if( debug ) {
+          std::cout << "VL53L0X Error: " << Status << std::endl;
+      }
+
+      return false;
+  }
+}
+
+/**************************************************************************/
+/*!
+    @brief  Change the I2C address of the sensor
+    @param  newAddr the new address to set the sensor to
+    @returns True if address was set successfully, False otherwise
+*/
+/**************************************************************************/
+bool FRC_VL53L0X::setAddress(uint8_t newAddr) {
+  newAddr &= 0x7F;
+
+  Status = VL53L0X_SetDeviceAddress(pMyDevice, newAddr * 2); // 7->8 bit
+
+  frc::WaitCommand(0.01);
+
+  if( Status == VL53L0X_ERROR_NONE ) {
+    pMyDevice->I2cDevAddr = newAddr;  // 7 bit addr
+    return true;
+  }
+  return false;
+}
+
+/**************************************************************************/
+/*!
+    @brief  get a ranging measurement from the device
+    @param  RangingMeasurementData the pointer to the struct the data will be stored in
+    @param debug Optional debug flag. If true debug information will print via Serial.print during execution. Defaults to false.
+    @returns True if address was set successfully, False otherwise
+*/
+/**************************************************************************/
+VL53L0X_Error FRC_VL53L0X::getSingleRangingMeasurement( VL53L0X_RangingMeasurementData_t *RangingMeasurementData, bool debug )
+{
+    VL53L0X_Error   Status = VL53L0X_ERROR_NONE;
+    FixPoint1616_t  LimitCheckCurrent;
+
+
+    /*
+     *  Step  4 : Test ranging mode
+     */
+
+    if( Status == VL53L0X_ERROR_NONE ) {
+        if( debug ) {
+            std::cout << "VL53L0X: PerformSingleRangingMeasurement" << std::endl;
+        }
+        Status = VL53L0X_PerformSingleRangingMeasurement( pMyDevice, RangingMeasurementData );
+
+        if( debug ) {
+            printRangeStatus( RangingMeasurementData );
+        }
+
+        if( debug ) {
+            VL53L0X_GetLimitCheckCurrent( pMyDevice, VL53L0X_CHECKENABLE_RANGE_IGNORE_THRESHOLD, &LimitCheckCurrent );
+
+          	std::cout << "RANGE IGNORE THRESHOLD: " << (LimitCheckCurrent / 65536.0 ) << std::endl;
+
+          	std::cout << "Measured distance: " << RangingMeasurementData->RangeMilliMeter << std::endl;
+        }
+    }
+
+    return Status;
+}
+
+
+
+/**************************************************************************/
+/*!
+    @brief  print a ranging measurement out via Serial.print in a human-readable format
+    @param pRangingMeasurementData a pointer to the ranging measurement data
+*/
+/**************************************************************************/
+void FRC_VL53L0X::printRangeStatus( VL53L0X_RangingMeasurementData_t* pRangingMeasurementData )
+{
+    char buf[ VL53L0X_MAX_STRING_LENGTH ];
+    uint8_t RangeStatus;
+
+    /*
+     * New Range Status: data is valid when pRangingMeasurementData->RangeStatus = 0
+     */
+
+    RangeStatus = pRangingMeasurementData->RangeStatus;
+
+    VL53L0X_GetRangeStatusString( RangeStatus, buf );
+
+    std::cout << "Range Status: " << RangeStatus << " : " << buf << std::endl;
+
+}
